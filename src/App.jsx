78ea82bc0690b8e15b4787fa6dc627e1c4cae1a2
Jugucan <judithg@outlook.com>
2025-10-05 09:52:04 +0200

    Error ordre llistes amb claude App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -77,6 +77,7 @@ function App() {
         uploadFromExcel,
         updateItemOrder,
         updateSectionOrder,
+        updateAllSectionsOrder,
         handleLogin,
         handleRegister,
         handlePasswordReset,
